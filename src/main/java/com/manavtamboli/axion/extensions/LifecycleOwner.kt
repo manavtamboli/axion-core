b68ed029f1720947f159a9577f66a80bb6d4d3b9
manavtamboli <manavtamboli8@gmail.com>
2021-07-19 00:32:00 +0530

    Lifecycle Owner Extensions for internal usage.

--- src/main/java/com/manavtamboli/axion/extensions/LifecycleOwner.kt
+++ src/main/java/com/manavtamboli/axion/extensions/LifecycleOwner.kt
@@ -1,6 +1,7 @@
 package com.manavtamboli.axion.extensions
 
 import android.content.Context
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
@@ -10,5 +11,11 @@ private class InvalidLifecycleOwnerException : Exception("Invalid LifecycleOwner
 internal val LifecycleOwner.context : Context get() = when (this){
     is Fragment -> requireContext()
     is ComponentActivity -> this
+    else -> throw InvalidLifecycleOwnerException()
+}
+
+internal val LifecycleOwner.window : Window get() = when(this) {
+    is Fragment -> requireActivity().window
+    is ComponentActivity -> window
     else -> throw InvalidLifecycleOwnerException()
 }
