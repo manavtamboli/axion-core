c29a62cc55b7453deb09f50370d536f759aa9438
manavtamboli <manavtamboli8@gmail.com>
2021-07-22 20:15:25 +0530

    Fixes and Dependency update

--- src/main/java/com/manavtamboli/axion/binding/components/BindingActivity.kt
+++ src/main/java/com/manavtamboli/axion/binding/components/BindingActivity.kt
@@ -2,12 +2,13 @@ package com.manavtamboli.axion.binding.components
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.manavtamboli.axion.binding.Binder
 import com.manavtamboli.axion.binding.BindingComponent
 import com.manavtamboli.axion.binding.BindingComponent.Companion.generateBinding
 
-open class BindingActivity<B : ViewBinding>(bindingClass : Class<B>) : ComponentActivity(), BindingComponent<B> {
+open class BindingActivity<B : ViewBinding>(bindingClass : Class<B>) : AppCompatActivity(), BindingComponent<B> {
 
     final override val binder = Binder.ofClass(bindingClass)
     override fun B.initialize() {}
--- src/main/java/com/manavtamboli/axion/ui/WindowColors.kt
+++ src/main/java/com/manavtamboli/axion/ui/WindowColors.kt
@@ -7,19 +7,19 @@ import com.manavtamboli.axion.core.Arc.Companion.context
 import com.manavtamboli.axion.extensions.window
 import com.manavtamboli.axion.lifecycle.doOnStart
 
-fun LifecycleOwner.navColor(@ColorRes color : Int){
+fun LifecycleOwner.navBarColor(@ColorRes color : Int){
     doOnStart {
         window.navigationBarColor = Arc.require().context.getColor(color)
     }
 }
 
-fun LifecycleOwner.statusColor(@ColorRes color: Int) {
+fun LifecycleOwner.statusBarColor(@ColorRes color: Int) {
     doOnStart {
         window.statusBarColor = Arc.require().context.getColor(color)
     }
 }
 
-fun LifecycleOwner.systemColor(@ColorRes color: Int){
+fun LifecycleOwner.systemBarsColor(@ColorRes color: Int){
     doOnStart {
         val colorInt = Arc.require().context.getColor(color)
         window.apply {
